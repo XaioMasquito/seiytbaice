49b0ffb936dee8bccf8959ebbd55f03a8488b3c5
new name <rt2767986@gmail.com>
2023-05-16 18:13:41 +0600

    seiytkazy

--- src/components/UI/Carus2.jsx
+++ src/components/UI/Carus2.jsx
@@ -33,18 +33,18 @@ export default function Carus2() {
         modules={[Grid, Pagination]}
         className="mySwiper2"
       >
-        <SwiperSlide><div className="mhum1"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum2"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum3"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum4"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum5"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum6"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum7"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum8"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum9"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum10"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum11"></div> </SwiperSlide>
-        <SwiperSlide><div className="mhum12"></div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_1"></div></div> <div className="min2"><a className="a_href" href="/">Flowers daisy pink stick</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_2"></div></div> <div className="min2"><a className="a_href" href="/">Blossom bouquet flower</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_3"></div></div> <div className="min2"><a className="a_href" href="/">Rose bouquet white</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_4"></div></div> <div className="min2"><a className="a_href" href="/">Glory of the Snow</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_5"></div></div> <div className="min2"><a className="a_href" href="/">Pearly Everlasting</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_6"></div></div> <div className="min2"><a className="a_href" href="/">Flowers white</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_7"></div></div> <div className="min2"><a className="a_href" href="/">Jasmine flowers white</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_8"></div></div> <div className="min2"><a className="a_href" href="/">Orchid flower red stick</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_9"></div></div> <div className="min2"><a className="a_href" href="/">Hyacinth white stick</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_10"></div></div> <div className="min2"><a className="a_href" href="/">Jack in the Pulpit</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_11"></div></div> <div className="min2"><a className="a_href" href="/">Flowers daisy pink stick</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
+        <SwiperSlide><div className="mhum"> <div className="min"><div className="min_12"></div></div> <div className="min2"><a className="a_href" href="/">Flower red stick</a><p className="min_text"></p><p className="p_min"><p className="min_text1">$80.00</p><p className="min_span"></p></p></div> </div> </SwiperSlide>
       </Swiper>
     </div>
   );
