b5dfa67c00d1d86dae6c6cc0a2d659223fabe243
marc <nurisnurlan1@gmail.com>
2023-05-18 19:19:42 +0600

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import "../../src/css/footer.scss"
 function Footer() {
   return (
     <div>
-      f
+      <footer>
+        <div class="parent6">
+          <div class="div61"> <div className='red_img'></div> <div className='red_p'><p className='red_p1'>Lorem Khaled Ipsum is a major key to success. To be successful you’ve got to work hard you’ve got to make it.</p></div>
+          <div className='red_imgblo'>
+            <div className="red_blo1"></div>
+            <div className="red_blo2"></div>
+            <div className="red_blo3"></div>
+            <div className="red_blo4"></div>
+            <div className="red_blo5"></div>
+          </div>
+          </div>
+          <div class="div62"> </div>
+          <div class="div63"> </div>
+          <div class="div64"> </div>
+          <div class="div65"> </div>
+        </div>
+      </footer>
     </div>
   )
 }
